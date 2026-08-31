e9d26f6b4953a0ce3c11a22eedf420e98f22bfe1
rubengitdev <rubencahyadi504@gmail.com>
2026-08-31 15:00:15 +0700

    Housekeeping: main

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-/**
- * React Application Mounting Core
- * 
- * Target element: '#root' from index.html.
- * Renders the top-level <App /> component wrapped under React.StrictMode to aid testing.
- */
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 );
